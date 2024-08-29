145322d3056eb0af246b5c1c22cc098ce267eb8a
xxxuuu <xxuuu0@outlook.com>
2024-08-29 21:16:29 +0800

    remove signin and signup pages
    Signed-off-by: xxxuuu <xxuuu0@outlook.com>

--- blog.config.js
+++ blog.config.js
@@ -121,8 +121,8 @@ const BLOG = {
     '/[prefix]': 'LayoutSlug',
     '/[prefix]/[slug]': 'LayoutSlug',
     '/[prefix]/[slug]/[...suffix]': 'LayoutSlug',
-    '/signin': 'LayoutSignIn',
-    '/signup': 'LayoutSignUp'
+    // '/signin': 'LayoutSignIn',
+    // '/signup': 'LayoutSignUp'
   },
 
   CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || true, // 是否允许复制页面内容 默认允许，如果设置为false、则全栈禁止复制内容。
